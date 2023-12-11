0bc8e84237246523d0e73ac9c16e528f2c3339db
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-11 18:00:17 +0530

    Drop table method added

---
+++ src/main/java/com/ivoyant/DropTable.java
@@ -0,0 +1,17 @@
+package com.ivoyant;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DropTable {
+    public static int drop(Connection connection, String tableName) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("DROP TABLE " + tableName);
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            return 0;
+        }
+    }
+}
