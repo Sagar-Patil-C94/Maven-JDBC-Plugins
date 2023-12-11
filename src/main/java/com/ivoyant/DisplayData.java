29b9918843740bb0f3c970277310e954d1f7698d
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-11 17:57:58 +0530

    Display Method Added

---
+++ src/main/java/com/ivoyant/DisplayData.java
@@ -0,0 +1,18 @@
+package com.ivoyant;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DisplayData {
+    public static ResultSet display(Connection connection, String columnNames, String tableName) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("SELECT " + columnNames + " FROM " + tableName);
+            return statement.getResultSet();
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            return null;
+        }
+    }
+}
