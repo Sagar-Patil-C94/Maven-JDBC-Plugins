abaf8ac48ac416443b64fcd0279e7ede8956b187
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-12 08:49:42 +0530

    Changed int return type to boolean in DropTable

--- src/main/java/com/ivoyant/DropTable.java
+++ src/main/java/com/ivoyant/DropTable.java
@@ -4,14 +4,14 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 public class DropTable {
-    public static int drop(Connection connection, String tableName) {
+    public static boolean drop(Connection connection, String tableName) {
         try {
             Statement statement = connection.createStatement();
             statement.execute(StringConstants.dropTablePrefix + tableName);
-            return 1;
+            return true;
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
-            return 0;
+            return false;
         }
     }
 }
