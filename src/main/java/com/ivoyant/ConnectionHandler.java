3866320e4d6bf8ff225ee560722b1e433db1efb4
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-12 18:50:58 +0530

    Added close connection method and categorized as connection handler

--- src/main/java/com/ivoyant/ConnectionHandler.java
+++ src/main/java/com/ivoyant/ConnectionHandler.java
@@ -3,8 +3,8 @@ package com.ivoyant;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class CreateConnection {
-    static Connection connect(String hostName, int portNumber, String databaseName, String username, String password) {
+public class ConnectionHandler {
+    public static Connection connect(String hostName, int portNumber, String databaseName, String username, String password) {
         try {
             Class.forName(StringConstants.driverName);
             return DriverManager.getConnection(StringConstants.urlPrefix + hostName + StringConstants.colon +
@@ -13,6 +13,15 @@ public class CreateConnection {
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
             return null;
+        }
+    }
+
+    public static boolean disconnect(Connection connection) {
+        try {
+            connection.close();
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 }
