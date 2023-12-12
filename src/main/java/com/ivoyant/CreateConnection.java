a67d4592a44834b8ffd2e8178c9ce80fa7938a48
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-12 08:28:56 +0530

    Added String Constants For CreateConnection

--- src/main/java/com/ivoyant/CreateConnection.java
+++ src/main/java/com/ivoyant/CreateConnection.java
@@ -6,8 +6,10 @@ import java.sql.DriverManager;
 public class CreateConnection {
     static Connection connect(String hostName, int portNumber, String databaseName, String username, String password) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://" + hostName + ":" + portNumber + "/" + databaseName, username, password);
+            Class.forName(StringConstants.driverName);
+            return DriverManager.getConnection(StringConstants.urlPrefix + hostName + StringConstants.colon +
+                            portNumber + StringConstants.forwardSlash + databaseName,
+                    username, password);
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
             return null;
---
+++ src/main/java/com/ivoyant/StringConstants.java
@@ -0,0 +1,8 @@
+package com.ivoyant;
+
+public class StringConstants {
+    public static final String driverName = "com.mysql.cj.jdbc.Driver";
+    public static final String urlPrefix = "jdbc:mysql://";
+    public  static final String colon = ":";
+    public  static final String forwardSlash  = "/";
+}
